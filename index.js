80e325f1be658c50032d8fc408669362686670b7
Stacie Rogers <stacierogers92@icloud.com>
2023-01-09 10:09:34 -0600

    commit tree 78aa2b204cbb18836dc9d0c9f8d082e04227f7a5

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor( Math.random() * 10) + 0
+const num3 = 20
+const num4 = 5
+const mod = num3 / num4
+const max = Math.max(5, 10, 15, 20)
